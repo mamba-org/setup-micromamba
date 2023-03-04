03f2a9f2dd224af7825b3d0ea0e72b481062f92c
Pavel Zwerschke <pavelzw@gmail.com>
2023-03-04 23:54:55 +0100

    Add micromamba download

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
-import * as https from 'https'
 import * as fs from 'fs/promises'
-import * as crypto from 'crypto'
 import * as core from '@actions/core'
-import { wait } from './wait'
+import decompress from 'decompress'
 import { PATHS, sha256, micromambaUrl } from './util'
 
 async function downloadMicromamba(url: string) {
   await fs.mkdir(PATHS.micromambaBinFolder, { recursive: true })
-  const tarBz2Path = PATHS.micromambaBin + '.tar.bz2'
-  core.debug(`Downloading micromamba from ${url} to ${tarBz2Path} ...`)
+  core.debug(`Downloading micromamba from ${url} ...`)
   fetch(url)
-    .then((response) => {
-      return response.arrayBuffer()
-    })
+    .then((response) => response.arrayBuffer())
     .then((buffer) => Buffer.from(buffer))
     .then((buffer) => {
-      fs.writeFile(tarBz2Path, buffer, 'binary')
-      core.debug(`Downloaded micromamba to ${tarBz2Path} ...`)
-      core.debug(`SHA256: ${sha256(buffer)}`)
+      core.debug(`.tar.bz2 sha256: ${sha256(buffer)}`)
+      return decompress(buffer, {
+        filter: (file) => file.path === 'bin/micromamba',
+        map: (file) => {
+          file.path = 'micromamba'
+          return file
+        }
+      })
+    })
+    .then((files) => {
+      const buffer = files[0].data
+      fs.writeFile(PATHS.micromambaBin, buffer, { encoding: 'binary', mode: 0o755 })
+      core.debug(`Downloaded micromamba executable to ${PATHS.micromambaBin} ...`)
     })
     .catch((err) => {
       core.error(`Error downloading file: ${err.message}`)
@@ -25,16 +30,6 @@ async function downloadMicromamba(url: string) {
 }
 
 const run = async () => {
-  // try {
-  //   const ms: string = core.getInput('milliseconds')
-  //   core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-  //   core.debug(new Date().toTimeString())
-  //   await wait(parseInt(ms, 10))
-  //   core.debug(new Date().toTimeString())
-  //   core.setOutput('time', new Date().toTimeString())
-  // } catch (error) {
-  //   if (error instanceof Error) core.setFailed(error.message)
-  // }
   await downloadMicromamba(micromambaUrl('osx-arm64', '1.3.0'))
 }
 
