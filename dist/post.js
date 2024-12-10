083f6b3b89ff07f719d9a31838b308f78cde1074
Johan Mabille <johan.mabille@gmail.com>
2024-12-10 16:28:45 +0100

    Changes according to review

--- dist/post.js
+++ dist/post.js
@@ -81846,7 +81846,7 @@ var getOptions = () => {
     ),
     micromambaVersion: parseOrUndefined(
       "micromamba-version",
-      unionType([literalType("latest"), stringType().regex(/^\d+\.\d+\.\d+(?:\.(?:rc|alpha|beta)\d+)?-\d+$/)]),
+      unionType([literalType("latest"), stringType().regex(/^\d+\.\d+\.\d+(?:\.?(?:rc|alpha|beta|dev)\d+)?-\d+$/)]),
       "micromamba-version must be either `latest` or a version matching `1.2.3-0`."
     ),
     micromambaUrl: parseOrUndefined("micromamba-url", stringType().url()),
